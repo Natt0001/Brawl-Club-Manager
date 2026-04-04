e9b68f9bb43409e803c0beec70b55c0e0e43e1b8
☁️ 𝓝𝓪𝓽𝓽 ☁️ <l.nathan.2007@gmail.com>
2026-04-04 15:42:22 +0200

    Delete brawl-clm directory

--- brawl-clm/sql/migrations/002_brawl_api_sync.sql
+++
@@ -1,9 +0,0 @@
-alter table if exists public.clubs
-  add column if not exists club_tag text unique;
-
-alter table if exists public.persons
-  add column if not exists display_name text;
-
-drop index if exists idx_persons_game_name_tag_unique;
-create unique index if not exists idx_persons_game_name_tag_unique
-on public.persons (game_name, coalesce(game_tag, ''));
